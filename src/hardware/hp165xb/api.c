e5ee43bba1f3e0e99e0d4d8fc4006046d3d9023a
lukken <lukken@astron.nl>
2025-11-01 19:06:58 +0100

    Non segfaulting dummy driver

--- src/hardware/hp165xb/api.c
+++ src/hardware/hp165xb/api.c
@@ -26,94 +26,41 @@
 #include "libsigrok-internal.h"
 #include "protocol.h"
 
-#define DEFAULT_NUM_LOGIC_CHANNELS		8
-#define DEFAULT_LOGIC_PATTERN			PATTERN_SIGROK
-
-#define DEFAULT_NUM_ANALOG_CHANNELS		5
-
 static const uint32_t scanopts[] = {
-	SR_CONF_NUM_LOGIC_CHANNELS,
-	SR_CONF_NUM_ANALOG_CHANNELS,
-	SR_CONF_LIMIT_FRAMES,
+	SR_CONF_CONN,
+	SR_CONF_SERIALCOMM
 };
 
 static const uint32_t drvopts[] = {
-	SR_CONF_DEMO_DEV,
-	SR_CONF_LOGIC_ANALYZER,
-	SR_CONF_OSCILLOSCOPE,
+	SR_CONF_LOGIC_ANALYZER
 };
 
 static const uint32_t devopts[] = {
-	SR_CONF_CONTINUOUS,
-	SR_CONF_LIMIT_SAMPLES | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_LIMIT_MSEC | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_LIMIT_FRAMES | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_SAMPLERATE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
-	SR_CONF_AVERAGING | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_AVG_SAMPLES | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_TRIGGER_MATCH | SR_CONF_LIST,
-	SR_CONF_CAPTURE_RATIO | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_CONN | SR_CONF_GET,
+	// SR_CONF_ENABLED | SR_CONF_SET,
 };
 
-static const uint32_t devopts_cg_logic[] = {
-	SR_CONF_PATTERN_MODE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+static const uint32_t devopts_cg[] = {
+	// SR_CONF_ENABLED | SR_CONF_SET,
 };
 
-static const uint32_t devopts_cg_analog_group[] = {
-	SR_CONF_AMPLITUDE | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_OFFSET | SR_CONF_GET | SR_CONF_SET,
-};
-
-static const uint32_t devopts_cg_analog_channel[] = {
-	SR_CONF_MEASURED_QUANTITY | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_PATTERN_MODE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
-	SR_CONF_AMPLITUDE | SR_CONF_GET | SR_CONF_SET,
-	SR_CONF_OFFSET | SR_CONF_GET | SR_CONF_SET,
-};
-
-static const int32_t trigger_matches[] = {
-	SR_TRIGGER_ZERO,
-	SR_TRIGGER_ONE,
-	SR_TRIGGER_RISING,
-	SR_TRIGGER_FALLING,
-	SR_TRIGGER_EDGE,
-};
-
-static const uint64_t samplerates[] = {
-	SR_HZ(1),
-	SR_GHZ(1),
-	SR_HZ(1),
-};
+// static const uint64_t samplerates[] = {
+// 	SR_HZ(1),
+// 	SR_GHZ(1),
+// 	SR_HZ(1),
+// };
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
 	struct dev_context *devc;
 	struct sr_dev_inst *sdi;
 	struct sr_channel *ch;
-	struct sr_channel_group *cg, *acg;
+	struct sr_channel_group *cg;
 	struct sr_config *src;
-	struct analog_gen *ag;
 	GSList *l;
-	int num_logic_channels, num_analog_channels, pattern, i;
+	int num_logic_channels = DEFAULT_NUM_LOGIC_CHANNELS;
 	uint64_t limit_frames;
 	char channel_name[16];
-
-	num_logic_channels = DEFAULT_NUM_LOGIC_CHANNELS;
-	num_analog_channels = DEFAULT_NUM_ANALOG_CHANNELS;
-	for (l = options; l; l = l->next) {
-		src = l->data;
-		switch (src->key) {
-		case SR_CONF_NUM_LOGIC_CHANNELS:
-			num_logic_channels = g_variant_get_int32(src->data);
-			break;
-		case SR_CONF_NUM_ANALOG_CHANNELS:
-			num_analog_channels = g_variant_get_int32(src->data);
-			break;
-		case SR_CONF_LIMIT_FRAMES:
-			limit_frames = g_variant_get_uint64(src->data);
-			break;
-		}
-	}
 
 	sdi = g_malloc0(sizeof(struct sr_dev_inst));
 	sdi->status = SR_ST_INACTIVE;
@@ -123,53 +70,111 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	devc->cur_samplerate = SR_KHZ(200);
 	devc->num_logic_channels = num_logic_channels;
 	devc->logic_unitsize = (devc->num_logic_channels + 7) / 8;
-	devc->all_logic_channels_mask = 1UL << 0;
-	devc->all_logic_channels_mask <<= devc->num_logic_channels;
-	devc->all_logic_channels_mask--;
-	devc->num_analog_channels = num_analog_channels;
-	devc->limit_frames = limit_frames;
+
+	if (num_logic_channels > 0) {
+		/* Logic channels, all in one channel group. */
+		cg = sr_channel_group_new(sdi, "Logic", NULL);
+		for (size_t i = 0; i < num_logic_channels; i++) {
+			sprintf(channel_name, "D%d", i);
+			ch = sr_channel_new(sdi, i, SR_CHANNEL_LOGIC, TRUE, channel_name);
+			cg->channels = g_slist_append(cg->channels, ch);
+		}
+	}
 
 	sdi->priv = devc;
 
 	return std_scan_complete(di, g_slist_append(NULL, sdi));
 }
 
-static void clear_helper(struct dev_context *devc)
-{
-
-}
-
-static int dev_clear(const struct sr_dev_driver *di)
-{
-	return std_dev_clear_with_callback(di, (std_dev_clear_callback)clear_helper);
-}
-
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
+	struct dev_context *devc;
 
+	if (!sdi)
+		return SR_ERR_ARG;
+
+	devc = sdi->priv;
+	switch (key) {
+	// case SR_CONF_ENABLED:
+	// 	*data = g_variant_new_uint64(sdi->connection_id);
+	// 	break;
+	default:
+		return SR_ERR_NA;
+	}
+
+	return SR_OK;
 }
 
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
+	struct dev_context *devc;
+	// struct analog_gen *ag;
+	// struct sr_channel *ch;
+	// GVariant *mq_tuple_child;
+	// GSList *l;
+	// int logic_pattern, analog_pattern;
 
+	devc = sdi->priv;
+
+	switch (key) {
+	// case SR_CONF_ENABLED:
+	// 	/* Enable/disable all channels at the same time. */
+	// 	on = g_variant_get_boolean(data);
+	// 	break;
+	// case SR_CONF_SAMPLERATE:
+	// 	devc->cur_samplerate = g_variant_get_uint64(data);
+	// 	break;
+	default:
+		return SR_ERR_NA;
+	}
+
+	return SR_OK;
 }
 
 static int config_list(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
+	struct sr_channel *ch;
+
+	if (!cg) {
+		switch (key) {
+		case SR_CONF_SCAN_OPTIONS:
+		case SR_CONF_DEVICE_OPTIONS:
+			return STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+		// case SR_CONF_SAMPLERATE:
+		// 	*data = std_gvar_samplerates_steps(ARRAY_AND_SIZE(samplerates));
+		// 	break;
+		// case SR_CONF_TRIGGER_MATCH:
+		// 	*data = std_gvar_array_i32(ARRAY_AND_SIZE(trigger_matches));
+		// 	break;
+		default:
+			return SR_ERR_NA;
+		}
+	} else {
+		ch = cg->channels->data;
+		switch (key) {
+		case SR_CONF_DEVICE_OPTIONS:
+			*data = std_gvar_array_u32(ARRAY_AND_SIZE(devopts_cg));
+			break;
+		default:
+			return SR_ERR_NA;
+		}
+	}
+
+	return SR_OK;
 }
 
-static int dev_acquisition_start(const struct sr_dev_inst *sdi)
-{
+// static int dev_acquisition_start(const struct sr_dev_inst *sdi)
+// {
 
-}
+// }
 
-static int dev_acquisition_stop(struct sr_dev_inst *sdi)
-{
+// static int dev_acquisition_stop(struct sr_dev_inst *sdi)
+// {
 
-}
+// }
 
 static struct sr_dev_driver hp165xb_driver_info = {
 	.name = "HP1650XB",
@@ -179,14 +184,15 @@ static struct sr_dev_driver hp165xb_driver_info = {
 	.cleanup = std_cleanup,
 	.scan = scan,
 	.dev_list = std_dev_list,
-	.dev_clear = dev_clear,
+	.dev_clear = std_dev_clear,
 	.config_get = config_get,
 	.config_set = config_set,
 	.config_list = config_list,
 	.dev_open = std_dummy_dev_open,
 	.dev_close = std_dummy_dev_close,
-	.dev_acquisition_start = dev_acquisition_start,
-	.dev_acquisition_stop = dev_acquisition_stop,
+	.dev_acquisition_start = std_dummy_dev_acquisition_start,
+	.dev_acquisition_stop = std_dummy_dev_acquisition_stop,
 	.context = NULL,
 };
+
 SR_REGISTER_DEV_DRIVER(hp165xb_driver_info);
--- src/hardware/hp165xb/protocol.h
+++ src/hardware/hp165xb/protocol.h
@@ -27,30 +27,52 @@
 
 #define LOG_PREFIX "HP165XB"
 
-#define DEFAULT_NUM_LOGIC_CHANNELS
+#define DEFAULT_NUM_LOGIC_CHANNELS 1
 
 struct dev_context {
 	uint64_t cur_samplerate;
 	uint64_t limit_samples;
 	uint64_t limit_msec;
-	uint64_t limit_frames;
+	// uint64_t limit_frames;
 	uint64_t sent_samples;
 	uint64_t sent_frame_samples; /* Number of samples that were sent for current frame. */
 	int64_t start_us;
 	int64_t spent_us;
 	uint64_t step;
 
+	/* Averaging */
+	// gboolean avg; /* True if averaging is enabled */
+	// uint64_t avg_samples;
+
 	/* Logic */
 	int32_t num_logic_channels;
 	size_t logic_unitsize;
-	uint64_t all_logic_channels_mask;
+	// uint64_t all_logic_channels_mask;
 	uint8_t logic_data[4096];
 
 	/* Analog */
-	int32_t num_analog_channels;
-	GHashTable *ch_ag;
-	size_t enabled_logic_channels;
-	size_t enabled_analog_channels;
+	// int32_t num_analog_channels;
+	// GHashTable *ch_ag;
+	// size_t enabled_logic_channels;
+	// size_t enabled_analog_channels;
+
+	/* Triggers */
+	// uint64_t capture_ratio;
 };
+
+// struct analog_gen {
+// 	struct sr_channel *ch;
+// 	enum sr_mq mq;
+// 	enum sr_mqflag mq_flags;
+// 	enum sr_unit unit;
+// 	float amplitude;
+// 	float offset;
+// 	struct sr_datafeed_analog packet;
+// 	struct sr_analog_encoding encoding;
+// 	struct sr_analog_meaning meaning;
+// 	struct sr_analog_spec spec;
+// 	float avg_val; /* Average value */
+// 	unsigned int num_avgs; /* Number of samples averaged */
+// };
 
 #endif
