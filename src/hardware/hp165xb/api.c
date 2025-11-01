22e8206cc22fae6de1550c256f6fb6944aacbb13
lukken <lukken@astron.nl>
2025-11-01 20:47:23 +0100

    Start of serial conversion

--- src/hardware/hp165xb/api.c
+++ src/hardware/hp165xb/api.c
@@ -44,47 +44,129 @@ static const uint32_t devopts_cg[] = {
 	// SR_CONF_ENABLED | SR_CONF_SET,
 };
 
-// static const uint64_t samplerates[] = {
-// 	SR_HZ(1),
-// 	SR_GHZ(1),
-// 	SR_HZ(1),
-// };
+static struct sr_dev_driver hp_1650b_driver_info;
+
+/**
+ * TODO(Corne): Fix me
+ */
+SR_PRIV int hp_1650b_get_model(struct sr_serial_dev_inst *serial,
+	uint8_t *model_code, uint8_t *hw_version, uint8_t *fw_version)
+{
+	return SR_ERR;
+	sr_log(SR_LOG_DBG, "Serial Scan!");
+}
+
+static struct sr_dev_inst *probe_device_conn(const char *conn)
+{
+	struct sr_dev_inst *sdi;
+	struct dev_context *devc;
+	struct sr_serial_dev_inst *ser;
+	uint8_t code, hwver, fwver;
+	char snr_txt[16];
+	struct channel_group_context *cgc;
+	size_t ch_idx, nr, do_idx, di_idx, ai_idx;
+	struct sr_channel_group *cg;
+	char cg_name[24];
+	int ret;
+
+	sdi = g_malloc0(sizeof(*sdi));
+	devc = g_malloc0(sizeof(*devc));
+	sdi->priv = devc;
+	ser = sr_serial_dev_inst_new(conn, NULL);
+	sdi->conn = ser;
+	if (!ser)
+		goto probe_fail;
+	ret = serial_open(ser, 0);
+	if (ret != SR_OK)
+		goto probe_fail;
+
+	ret = hp_1650b_get_model(ser, &code, &hwver, &fwver);
+	if (ret != SR_OK)
+		goto probe_fail;
+
+	sdi->vendor = g_strdup("HP");
+	// sdi->model = g_strdup(model->name);
+	// sdi->version = g_strdup_printf("HW%u FW%u", hwver, fwver);
+	sdi->connection_id = g_strdup(conn);
+	sdi->driver = &hp_1650b_driver_info;
+	sdi->inst_type = SR_INST_SERIAL;
+
+	return sdi;
+
+probe_fail:
+	if (ser) {
+		serial_close(ser);
+		sr_serial_dev_inst_free(ser);
+	}
+	if (devc) {
+		g_free(devc);
+	}
+	if (sdi) {
+		sdi->priv = NULL;
+		sr_dev_inst_free(sdi);
+		sdi = NULL;
+	}
+	return sdi;
+}
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	struct dev_context *devc;
+	struct drv_context *drvc;
+	const char *conn;
+	GSList *devices;
 	struct sr_dev_inst *sdi;
-	struct sr_channel *ch;
-	struct sr_channel_group *cg;
-	struct sr_config *src;
-	GSList *l;
-	int num_logic_channels = DEFAULT_NUM_LOGIC_CHANNELS;
-	uint64_t limit_frames;
-	char channel_name[16];
 
-	sdi = g_malloc0(sizeof(struct sr_dev_inst));
-	sdi->status = SR_ST_INACTIVE;
-	sdi->model = g_strdup("Psuedo HP1650b");
+	drvc = di->context;
+	drvc->instances = NULL;
 
-	devc = g_malloc0(sizeof(struct dev_context));
-	devc->cur_samplerate = SR_KHZ(200);
-	devc->num_logic_channels = num_logic_channels;
-	devc->logic_unitsize = (devc->num_logic_channels + 7) / 8;
+	conn = NULL;
+	(void)sr_serial_extract_options(options, &conn, NULL);
+	if (!conn || !*conn)
+		return NULL;
 
-	if (num_logic_channels > 0) {
-		/* Logic channels, all in one channel group. */
-		cg = sr_channel_group_new(sdi, "Logic", NULL);
-		for (size_t i = 0; i < num_logic_channels; i++) {
-			sprintf(channel_name, "D%d", i);
-			ch = sr_channel_new(sdi, i, SR_CHANNEL_LOGIC, TRUE, channel_name);
-			cg->channels = g_slist_append(cg->channels, ch);
-		}
-	}
+	devices = NULL;
+	sdi = probe_device_conn(conn);
+	if (sdi)
+		devices = g_slist_append(devices, sdi);
 
-	sdi->priv = devc;
-
-	return std_scan_complete(di, g_slist_append(NULL, sdi));
+	return std_scan_complete(di, devices);
 }
+
+// static GSList *scan(struct sr_dev_driver *di, GSList *options)
+// {
+// 	struct dev_context *devc;
+// 	struct sr_dev_inst *sdi;
+// 	struct sr_channel *ch;
+// 	struct sr_channel_group *cg;
+// 	// struct sr_config *src;
+// 	// GSList *l;
+// 	int num_logic_channels = DEFAULT_NUM_LOGIC_CHANNELS;
+// 	// uint64_t limit_frames;
+// 	char channel_name[16];
+
+// 	sdi = g_malloc0(sizeof(struct sr_dev_inst));
+// 	sdi->status = SR_ST_INACTIVE;
+// 	sdi->model = g_strdup("Psuedo HP1650b");
+
+// 	devc = g_malloc0(sizeof(struct dev_context));
+// 	devc->cur_samplerate = SR_KHZ(200);
+// 	devc->num_logic_channels = num_logic_channels;
+// 	devc->logic_unitsize = (devc->num_logic_channels + 7) / 8;
+
+// 	if (num_logic_channels > 0) {
+// 		/* Logic channels, all in one channel group. */
+// 		cg = sr_channel_group_new(sdi, "Logic", NULL);
+// 		for (size_t i = 0; i < num_logic_channels; i++) {
+// 			sprintf(channel_name, "D%d", i);
+// 			ch = sr_channel_new(sdi, i, SR_CHANNEL_LOGIC, TRUE, channel_name);
+// 			cg->channels = g_slist_append(cg->channels, ch);
+// 		}
+// 	}
+
+// 	sdi->priv = devc;
+
+// 	return std_scan_complete(di, g_slist_append(NULL, sdi));
+// }
 
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
@@ -177,8 +259,8 @@ static int config_list(uint32_t key, GVariant **data,
 // }
 
 static struct sr_dev_driver hp165xb_driver_info = {
-	.name = "HP1650XB",
-	.longname = "Driver for HP 1650B series logic analyzers",
+	.name = "HP1650B",
+	.longname = "HP 1650B logic analyzers",
 	.api_version = 1,
 	.init = std_init,
 	.cleanup = std_cleanup,
